d46a11169dabd6331cf1a6e9dd60967cf9d04240
ranjith <apple@apples-MacBook-Pro.local>
2025-12-15 12:44:23 +0530

    removed ssl verification

--- src/config/db.js
+++ src/config/db.js
@@ -8,10 +8,7 @@ const pool = mysql.createPool({
   database: process.env.DB_NAME || "smfinace",
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: true,
-  },
+  queueLimit: 0
 });
 
 module.exports = pool;
